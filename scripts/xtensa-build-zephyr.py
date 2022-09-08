34feb97916b366f4c4c7db4e1bff3ef87be89349
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2022-09-08 18:38:03 +0200

    key patch correction to sof
    sof is no longer in modules/audio/sof
    
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

--- scripts/xtensa-build-zephyr.py
+++ scripts/xtensa-build-zephyr.py
@@ -93,14 +93,14 @@ platform_list = [
 		"PLAT_CONFIG": "intel_adsp_ace15_mtpm",
 		"XTENSA_CORE": "ace10_LX7HiFi4_RI_2020_5",
 		"XTENSA_TOOLS_VERSION": f"RI-2020.5{xtensa_tools_version_postfix}",
-		"RIMAGE_KEY": pathlib.Path("modules", "audio", "sof", "keys", "mtl_private_key.pem")
+		"RIMAGE_KEY": pathlib.Path("sof", "keys", "mtl_private_key.pem")
 	},
 	{
 		"name": "mtl-sim",
 		"PLAT_CONFIG": "intel_adsp_ace15_mtpm_sim",
 		"XTENSA_CORE": "ace10_LX7HiFi4_RI_2020_5",
 		"XTENSA_TOOLS_VERSION": f"RI-2020.5{xtensa_tools_version_postfix}",
-		"RIMAGE_KEY": pathlib.Path("modules", "audio", "sof", "keys", "mtl_private_key.pem")
+		"RIMAGE_KEY": pathlib.Path("sof", "keys", "mtl_private_key.pem")
 	},
 	# NXP platforms
 	{
