pjdobrowolski/sof_zephyr

[EMBARGO] squash commit: add ace folder under platform/intel
Add ace platform (cavs copy with modifications) under platform/intel
Contains the latest versions of files before rebase

Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

src/platform/intel/ace/include/ace/drivers/dw-dma.h
@@ -1,0 +1,110 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifdef __PLATFORM_DRIVERS_DW_DMA_H__
+
+#ifndef __ACE_LIB_DW_DMA_H__
+#define __ACE_LIB_DW_DMA_H__
+
+#include <sof/bit.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/shim.h>
+
+#include <stdint.h>
+
+/* number of supported DW-DMACs */
+#define PLATFORM_NUM_DW_DMACS	2
+
+/* index of the first DW-DMAC in the array */
+#define PLATFORM_DW_DMA_INDEX	0
+
+/* DMA treats PHY addresses as host address unless within DSP region */
+#define PLATFORM_DW_DMA_HOST_MASK	0x00000000
+
+/* CTL_HI */
+#define DW_CTLH_CLASS(x)	SET_BITS(31, 29, x)
+#define DW_CTLH_WEIGHT(x)	SET_BITS(28, 18, x)
+#define DW_CTLH_DONE(x)		SET_BIT(17, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(16, 0)
+
+/* CFG_LO */
+#define DW_CFG_RELOAD_DST	BIT(31)
+#define DW_CFG_RELOAD_SRC	BIT(30)
+#define DW_CFG_CTL_HI_UPD_EN	BIT(5)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER_EXT(x)		SET_BITS(31, 30, x)
+#define DW_CFGH_SRC_PER_EXT(x)		SET_BITS(29, 28, x)
+#define DW_CFGH_DST_PER(x)		SET_BITS(7, 4, x)
+#define DW_CFGH_SRC_PER(x)		SET_BITS(3, 0, x)
+#define DW_CFGH_DST(x) \
+	(DW_CFGH_DST_PER_EXT((x) >> 4) | DW_CFGH_DST_PER(x))
+#define DW_CFGH_SRC(x) \
+	(DW_CFGH_SRC_PER_EXT((x) >> 4) | DW_CFGH_SRC_PER(x))
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x3
+#define DW_CFG_HIGH_DEF		0x0
+
+/* LLPC address */
+#define DW_CHLLPC(dma, chan) \
+	SHIM_GPDMA_CHLLPC((dma)->plat_data.id, (chan)->index)
+
+#define DW_CHLLPL(dma, chan) \
+	SHIM_GPDMA_CHLLPL((dma)->plat_data.id, (chan)->index)
+
+#define DW_CHLLPU(dma, chan) \
+	SHIM_GPDMA_CHLLPU((dma)->plat_data.id, (chan)->index)
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(lli->ctrl_hi |= DW_CTLH_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= (size & DW_CTLH_BLOCK_TS_MASK))
+
+static inline void platform_dw_dma_llp_config(struct dma *dma,
+					      struct dma_chan_data *chan,
+					      uint32_t config)
+{
+	shim_write(DW_CHLLPC(dma, chan), SHIM_GPDMA_CHLLPC_DHRS(config));
+}
+
+static inline void platform_dw_dma_llp_enable(struct dma *dma,
+					      struct dma_chan_data *chan)
+{
+	shim_write(DW_CHLLPC(dma, chan),
+		   shim_read(DW_CHLLPC(dma, chan)) | SHIM_GPDMA_CHLLPC_EN);
+}
+
+static inline void platform_dw_dma_llp_disable(struct dma *dma,
+					       struct dma_chan_data *chan)
+{
+	shim_write(DW_CHLLPC(dma, chan),
+		   shim_read(DW_CHLLPC(dma, chan)) & ~SHIM_GPDMA_CHLLPC_EN);
+}
+
+static inline void platform_dw_dma_llp_read(struct dma *dma,
+					    struct dma_chan_data *chan,
+					    uint32_t *llp_l,
+					    uint32_t *llp_u)
+{
+	*llp_l = shim_read(DW_CHLLPL(dma, chan));
+	*llp_u = shim_read(DW_CHLLPU(dma, chan));
+}
+
+static inline struct dw_lli *platform_dw_dma_lli_get(struct dw_lli *lli)
+{
+	return cache_to_uncache(lli);
+}
+
+#endif /* __ACE_LIB_DW_DMA_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of platform/drivers/dw-dma.h"
+
+#endif /* __PLATFORM_DRIVERS_DW_DMA_H__ */

src/platform/intel/ace/include/ace/drivers/sideband-ipc.h
@@ -1,0 +1,46 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Rander Wang <rander.wang@intel.com>
+ */
+
+/**
+ * \file platform/intel/ace/include/ace/drivers/sideband-ipc.h
+ * \brief Defines layout of IPC registers for cAVS platforms with sideband IPC.
+ */
+
+#ifndef __ACE_DRIVERS_SIDEBAND_IPC_H__
+#define __ACE_DRIVERS_SIDEBAND_IPC_H__
+
+#include <sof/bit.h>
+
+/* DIPCTDR */
+#define IPC_DIPCTDR_BUSY	BIT(31)
+#define IPC_DIPCTDR_MSG_MASK	MASK(30, 0)
+
+/* DIPCTDA - ack from target to initiator */
+#define IPC_DIPCTDA_DONE	BIT(31)
+#define IPC_DIPCTDA_MSG_MASK	MASK(30, 0)
+
+/* DIPCTDD */
+#define IPC_DIPCTDD_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDA*/
+#define IPC_DIPCIDA_DONE	BIT(31)
+#define IPC_DIPCIDA_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDR */
+#define IPC_DIPCIDR_BUSY	BIT(31)
+#define IPC_DIPCIDR_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDD */
+#define IPC_DIPCIDD_MSG_MASK	MASK(31, 0)
+
+/* DIPCCTL */
+#define IPC_DIPCCTL_IPCIDIE	BIT(1)
+#define IPC_DIPCCTL_IPCTBIE	BIT(0)
+
+#endif /* __ACE_DRIVERS_SIDEBAND_IPC_H__ */

src/platform/intel/ace/include/ace/lib/mailbox.h
@@ -1,0 +1,123 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ */
+
+#ifdef __PLATFORM_LIB_MAILBOX_H__
+
+#ifndef __ACE_LIB_MAILBOX_H__
+#define __ACE_LIB_MAILBOX_H__
+
+#include <sof/debug/panic.h>
+#include <sof/lib/memory.h>
+#include <sof/string.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * The Window Region on HPSRAM for cAVS platforms is organised like
+ * this. The actual region order is platform-specific, see memory.h
+ * files.
+ *
+ * +--------------------------------------------------------------------------+
+ * | Offset              | Region         |  Size                             |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_TRACE_BASE     | Trace Buffer W3|  SRAM_TRACE_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_DEBUG_BASE     | Debug data  W2 |  SRAM_DEBUG_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_INBOX_BASE     | Inbox  W1      |  SRAM_INBOX_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_OUTBOX_BASE    | Outbox W0      |  SRAM_MAILBOX_SIZE                |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_SW_REG_BASE    | SW Registers W0|  SRAM_SW_REG_SIZE                 |
+ * +---------------------+----------------+-----------------------------------+
+ *
+ * Note: For suecreek SRAM_SW_REG window does not exist - MAILBOX_SW_REG_BASE
+ *	 and MAILBOX_SW_REG_BASE are equal to 0
+ */
+
+ /* window 3 - trace */
+#define MAILBOX_TRACE_SIZE	SRAM_TRACE_SIZE
+#define MAILBOX_TRACE_BASE	SRAM_TRACE_BASE
+
+ /* window 2 debug, exception and stream */
+#define MAILBOX_DEBUG_SIZE	SRAM_DEBUG_SIZE
+#define MAILBOX_DEBUG_BASE	SRAM_DEBUG_BASE
+
+#define MAILBOX_EXCEPTION_SIZE	SRAM_EXCEPT_SIZE
+#define MAILBOX_EXCEPTION_BASE	SRAM_EXCEPT_BASE
+#define MAILBOX_EXCEPTION_OFFSET  SRAM_DEBUG_SIZE
+
+#define MAILBOX_STREAM_SIZE    SRAM_STREAM_SIZE
+#define MAILBOX_STREAM_BASE    SRAM_STREAM_BASE
+#define MAILBOX_STREAM_OFFSET  (SRAM_DEBUG_SIZE + SRAM_EXCEPT_SIZE)
+
+ /* window 1 inbox/downlink and FW registers */
+#define MAILBOX_HOSTBOX_SIZE	SRAM_INBOX_SIZE
+#define MAILBOX_HOSTBOX_BASE	SRAM_INBOX_BASE
+
+ /* window 0 */
+#define MAILBOX_DSPBOX_SIZE	SRAM_OUTBOX_SIZE
+#define MAILBOX_DSPBOX_BASE	SRAM_OUTBOX_BASE
+
+#define MAILBOX_SW_REG_SIZE	SRAM_SW_REG_SIZE
+#define MAILBOX_SW_REG_BASE	SRAM_SW_REG_BASE
+
+static inline void mailbox_sw_reg_write(size_t offset, uint32_t src)
+{
+	volatile uint32_t *ptr;
+
+	ptr = (volatile uint32_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+	*ptr = src;
+}
+
+static inline void mailbox_sw_reg_write64(size_t offset, uint64_t src)
+{
+	volatile uint64_t *ptr;
+
+	ptr = (volatile uint64_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+	*ptr = src;
+}
+
+static inline uint32_t mailbox_sw_reg_read(size_t offset)
+{
+	volatile uint32_t *ptr;
+
+	ptr = (volatile uint32_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+
+	return *ptr;
+}
+
+static inline uint64_t mailbox_sw_reg_read64(size_t offset)
+{
+	volatile uint64_t *ptr;
+
+	ptr = (volatile uint64_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+
+	return *ptr;
+}
+
+static inline void mailbox_sw_regs_write(size_t offset, const void *src, size_t bytes)
+{
+	int ret = memcpy_s((void *)(MAILBOX_SW_REG_BASE + offset),
+			   MAILBOX_SW_REG_SIZE - offset, src, bytes);
+
+	assert(!ret);
+	dcache_writeback_region((void *)(MAILBOX_SW_REG_BASE + offset), bytes);
+}
+
+#endif /* __ACE_LIB_MAILBOX_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of platform/lib/mailbox.h"
+
+#endif /* __PLATFORM_LIB_MAILBOX_H__ */

src/platform/intel/ace/lib/dma.c
@@ -1,0 +1,198 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2022 Intel Corporation. All rights reserved.
+//
+// Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+//         Keyon Jie <yang.jie@linux.intel.com>
+//         Rander Wang <rander.wang@intel.com>
+//         Janusz Jankowski <janusz.jankowski@linux.intel.com>
+
+#include <sof/common.h>
+#include <sof/drivers/dw-dma.h>
+#include <sof/drivers/hda-dma.h>
+#include <sof/drivers/interrupt.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/memory.h>
+#include <sof/sof.h>
+#include <sof/spinlock.h>
+
+#define DMAC0_CLASS 6
+#define DMAC1_CLASS 7
+#define DMAC_HOST_IN_CHANNELS_COUNT 7
+#define DMAC_HOST_OUT_CHANNELS_COUNT 9
+#define DMAC_LINK_IN_CHANNELS_COUNT 7
+#define DMAC_LINK_OUT_CHANNELS_COUNT 9
+
+const struct dw_drv_plat_data dmac0 = {
+	.chan[0] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[1] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[2] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[3] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[4] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[5] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[6] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[7] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+};
+
+const struct dw_drv_plat_data dmac1 = {
+	.chan[0] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[1] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[2] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[3] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[4] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[5] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[6] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[7] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+};
+
+SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
+{	/* Low Power GP DMAC 0 */
+	.plat_data = {
+		.id		= DMA_GP_LP_DMAC0,
+		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
+				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV,
+		.caps		= DMA_CAP_GP_LP,
+		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC |
+				  DMA_DEV_ALH,
+		.base		= LP_GP_DMA_BASE(0),
+		.channels	= 8,
+		.irq		= IRQ_EXT_LP_GPDMA0_LVL5(0),
+		.irq_name	= irq_name_level5,
+		.drv_plat_data	= &dmac0,
+	},
+	.ops		= &dw_dma_ops,
+},
+{	/* Low Power GP DMAC 1 */
+	.plat_data = {
+		.id		= DMA_GP_LP_DMAC1,
+		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
+				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV,
+		.caps		= DMA_CAP_GP_LP,
+		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC |
+				  DMA_DEV_ALH,
+		.base		= LP_GP_DMA_BASE(1),
+		.channels	= 8,
+		.irq		= IRQ_EXT_LP_GPDMA1_LVL5(0),
+		.irq_name	= irq_name_level5,
+		.drv_plat_data	= &dmac1,
+	},
+	.ops		= &dw_dma_ops,
+},
+{	/* Host In DMAC */
+	.plat_data = {
+		.id		= DMA_HOST_IN_DMAC,
+		.dir		= DMA_DIR_LMEM_TO_HMEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HOST,
+		.base		= GTW_HOST_IN_STREAM_BASE(0),
+		.channels	= DMAC_HOST_IN_CHANNELS_COUNT,
+		.chan_size	= GTW_HOST_IN_STREAM_SIZE,
+	},
+	.ops		= &hda_host_dma_ops,
+},
+{	/* Host out DMAC */
+	.plat_data = {
+		.id		= DMA_HOST_OUT_DMAC,
+		.dir		= DMA_DIR_HMEM_TO_LMEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HOST,
+		.base		= GTW_HOST_OUT_STREAM_BASE(0),
+		.channels	= DMAC_HOST_OUT_CHANNELS_COUNT,
+		.chan_size	= GTW_HOST_OUT_STREAM_SIZE,
+	},
+	.ops		= &hda_host_dma_ops,
+},
+{	/* Link In DMAC */
+	.plat_data = {
+		.id		= DMA_LINK_IN_DMAC,
+		.dir		= DMA_DIR_DEV_TO_MEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HDA,
+		.base		= GTW_LINK_IN_STREAM_BASE(0),
+		.channels	= DMAC_LINK_IN_CHANNELS_COUNT,
+		.chan_size	= GTW_LINK_IN_STREAM_SIZE,
+	},
+	.ops		= &hda_link_dma_ops,
+},
+{	/* Link out DMAC */
+	.plat_data = {
+		.id		= DMA_LINK_OUT_DMAC,
+		.dir		= DMA_DIR_MEM_TO_DEV,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HDA,
+		.base		= GTW_LINK_OUT_STREAM_BASE(0),
+		.channels	= DMAC_LINK_OUT_CHANNELS_COUNT,
+		.chan_size	= GTW_LINK_OUT_STREAM_SIZE,
+	},
+	.ops		= &hda_link_dma_ops,
+},};
+
+const struct dma_info lib_dma = {
+	.dma_array = cache_to_uncache_init((struct dma *)dma),
+	.num_dmas = ARRAY_SIZE(dma)
+};
+
+/* Initialize all platform DMAC's */
+int dmac_init(struct sof *sof)
+{
+	int i;
+	/* no probing before first use */
+
+	/* TODO: dynamic init based on platform settings */
+
+	sof->dma_info = &lib_dma;
+
+	/* early lock initialization for ref counting */
+	for (i = 0; i < sof->dma_info->num_dmas; i++)
+		k_spinlock_init(&sof->dma_info->dma_array[i].lock);
+
+	return 0;
+}

kfrydryx/sof

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

src/platform/intel/ace/include/ace/drivers/dw-dma.h
@@ -1,0 +1,110 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifdef __PLATFORM_DRIVERS_DW_DMA_H__
+
+#ifndef __ACE_LIB_DW_DMA_H__
+#define __ACE_LIB_DW_DMA_H__
+
+#include <rtos/bit.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/shim.h>
+
+#include <stdint.h>
+
+/* number of supported DW-DMACs */
+#define PLATFORM_NUM_DW_DMACS	2
+
+/* index of the first DW-DMAC in the array */
+#define PLATFORM_DW_DMA_INDEX	0
+
+/* DMA treats PHY addresses as host address unless within DSP region */
+#define PLATFORM_DW_DMA_HOST_MASK	0x00000000
+
+/* CTL_HI */
+#define DW_CTLH_CLASS(x)	SET_BITS(31, 29, x)
+#define DW_CTLH_WEIGHT(x)	SET_BITS(28, 18, x)
+#define DW_CTLH_DONE(x)		SET_BIT(17, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(16, 0)
+
+/* CFG_LO */
+#define DW_CFG_RELOAD_DST	BIT(31)
+#define DW_CFG_RELOAD_SRC	BIT(30)
+#define DW_CFG_CTL_HI_UPD_EN	BIT(5)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER_EXT(x)		SET_BITS(31, 30, x)
+#define DW_CFGH_SRC_PER_EXT(x)		SET_BITS(29, 28, x)
+#define DW_CFGH_DST_PER(x)		SET_BITS(7, 4, x)
+#define DW_CFGH_SRC_PER(x)		SET_BITS(3, 0, x)
+#define DW_CFGH_DST(x) \
+	(DW_CFGH_DST_PER_EXT((x) >> 4) | DW_CFGH_DST_PER(x))
+#define DW_CFGH_SRC(x) \
+	(DW_CFGH_SRC_PER_EXT((x) >> 4) | DW_CFGH_SRC_PER(x))
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x3
+#define DW_CFG_HIGH_DEF		0x0
+
+/* LLPC address */
+#define DW_CHLLPC(dma, chan) \
+	SHIM_GPDMA_CHLLPC((dma)->plat_data.id, (chan)->index)
+
+#define DW_CHLLPL(dma, chan) \
+	SHIM_GPDMA_CHLLPL((dma)->plat_data.id, (chan)->index)
+
+#define DW_CHLLPU(dma, chan) \
+	SHIM_GPDMA_CHLLPU((dma)->plat_data.id, (chan)->index)
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(lli->ctrl_hi |= DW_CTLH_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= (size & DW_CTLH_BLOCK_TS_MASK))
+
+static inline void platform_dw_dma_llp_config(struct dma *dma,
+					      struct dma_chan_data *chan,
+					      uint32_t config)
+{
+	shim_write(DW_CHLLPC(dma, chan), SHIM_GPDMA_CHLLPC_DHRS(config));
+}
+
+static inline void platform_dw_dma_llp_enable(struct dma *dma,
+					      struct dma_chan_data *chan)
+{
+	shim_write(DW_CHLLPC(dma, chan),
+		   shim_read(DW_CHLLPC(dma, chan)) | SHIM_GPDMA_CHLLPC_EN);
+}
+
+static inline void platform_dw_dma_llp_disable(struct dma *dma,
+					       struct dma_chan_data *chan)
+{
+	shim_write(DW_CHLLPC(dma, chan),
+		   shim_read(DW_CHLLPC(dma, chan)) & ~SHIM_GPDMA_CHLLPC_EN);
+}
+
+static inline void platform_dw_dma_llp_read(struct dma *dma,
+					    struct dma_chan_data *chan,
+					    uint32_t *llp_l,
+					    uint32_t *llp_u)
+{
+	*llp_l = shim_read(DW_CHLLPL(dma, chan));
+	*llp_u = shim_read(DW_CHLLPU(dma, chan));
+}
+
+static inline struct dw_lli *platform_dw_dma_lli_get(struct dw_lli *lli)
+{
+	return cache_to_uncache(lli);
+}
+
+#endif /* __ACE_LIB_DW_DMA_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of platform/drivers/dw-dma.h"
+
+#endif /* __PLATFORM_DRIVERS_DW_DMA_H__ */

src/platform/intel/ace/include/ace/drivers/sideband-ipc.h
@@ -1,0 +1,46 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Rander Wang <rander.wang@intel.com>
+ */
+
+/**
+ * \file platform/intel/ace/include/ace/drivers/sideband-ipc.h
+ * \brief Defines layout of IPC registers for cAVS platforms with sideband IPC.
+ */
+
+#ifndef __ACE_DRIVERS_SIDEBAND_IPC_H__
+#define __ACE_DRIVERS_SIDEBAND_IPC_H__
+
+#include <rtos/bit.h>
+
+/* DIPCTDR */
+#define IPC_DIPCTDR_BUSY	BIT(31)
+#define IPC_DIPCTDR_MSG_MASK	MASK(30, 0)
+
+/* DIPCTDA - ack from target to initiator */
+#define IPC_DIPCTDA_DONE	BIT(31)
+#define IPC_DIPCTDA_MSG_MASK	MASK(30, 0)
+
+/* DIPCTDD */
+#define IPC_DIPCTDD_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDA*/
+#define IPC_DIPCIDA_DONE	BIT(31)
+#define IPC_DIPCIDA_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDR */
+#define IPC_DIPCIDR_BUSY	BIT(31)
+#define IPC_DIPCIDR_MSG_MASK	MASK(30, 0)
+
+/* DIPCIDD */
+#define IPC_DIPCIDD_MSG_MASK	MASK(31, 0)
+
+/* DIPCCTL */
+#define IPC_DIPCCTL_IPCIDIE	BIT(1)
+#define IPC_DIPCCTL_IPCTBIE	BIT(0)
+
+#endif /* __ACE_DRIVERS_SIDEBAND_IPC_H__ */

src/platform/intel/ace/include/ace/lib/mailbox.h
@@ -1,0 +1,124 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ */
+
+#ifdef __PLATFORM_LIB_MAILBOX_H__
+
+#ifndef __ACE_LIB_MAILBOX_H__
+#define __ACE_LIB_MAILBOX_H__
+
+#include <sof/debug/panic.h>
+#include <sof/lib/memory.h>
+#include <rtos/string.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * The Window Region on HPSRAM for cAVS platforms is organised like
+ * this. The actual region order is platform-specific, see memory.h
+ * files.
+ *
+ * +--------------------------------------------------------------------------+
+ * | Offset              | Region         |  Size                             |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_TRACE_BASE     | Trace Buffer W3|  SRAM_TRACE_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_DEBUG_BASE     | Debug data  W2 |  SRAM_DEBUG_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_INBOX_BASE     | Inbox  W1      |  SRAM_INBOX_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_OUTBOX_BASE    | Outbox W0      |  SRAM_MAILBOX_SIZE                |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_SW_REG_BASE    | SW Registers W0|  SRAM_SW_REG_SIZE                 |
+ * +---------------------+----------------+-----------------------------------+
+ *
+ * Note: For suecreek SRAM_SW_REG window does not exist - MAILBOX_SW_REG_BASE
+ *	 and MAILBOX_SW_REG_BASE are equal to 0
+ */
+
+ /* window 3 - trace */
+#define MAILBOX_TRACE_SIZE	SRAM_TRACE_SIZE
+#define MAILBOX_TRACE_BASE	SRAM_TRACE_BASE
+
+ /* window 2 debug, exception and stream */
+#define MAILBOX_DEBUG_SIZE	SRAM_DEBUG_SIZE
+#define MAILBOX_DEBUG_BASE	SRAM_DEBUG_BASE
+
+#define MAILBOX_EXCEPTION_SIZE	SRAM_EXCEPT_SIZE
+#define MAILBOX_EXCEPTION_BASE	SRAM_EXCEPT_BASE
+#define MAILBOX_EXCEPTION_OFFSET  SRAM_DEBUG_SIZE
+
+#define MAILBOX_STREAM_SIZE    SRAM_STREAM_SIZE
+#define MAILBOX_STREAM_BASE    SRAM_STREAM_BASE
+#define MAILBOX_STREAM_OFFSET  (SRAM_DEBUG_SIZE + SRAM_EXCEPT_SIZE)
+
+ /* window 1 inbox/downlink and FW registers */
+#define MAILBOX_HOSTBOX_SIZE	SRAM_INBOX_SIZE
+#define MAILBOX_HOSTBOX_BASE	SRAM_INBOX_BASE
+
+ /* window 0 */
+#define MAILBOX_DSPBOX_SIZE	SRAM_OUTBOX_SIZE
+#define MAILBOX_DSPBOX_BASE	SRAM_OUTBOX_BASE
+
+#define MAILBOX_SW_REG_SIZE	SRAM_SW_REG_SIZE
+#define MAILBOX_SW_REG_BASE	SRAM_SW_REG_BASE
+
+static inline void mailbox_sw_reg_write(size_t offset, uint32_t src)
+{
+	volatile uint32_t *ptr;
+
+	ptr = (volatile uint32_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+	*ptr = src;
+}
+
+static inline void mailbox_sw_reg_write64(size_t offset, uint64_t src)
+{
+	volatile uint64_t *ptr;
+
+	ptr = (volatile uint64_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+	*ptr = src;
+}
+
+static inline uint32_t mailbox_sw_reg_read(size_t offset)
+{
+	volatile uint32_t *ptr;
+
+	ptr = (volatile uint32_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+
+	return *ptr;
+}
+
+static inline uint64_t mailbox_sw_reg_read64(size_t offset)
+{
+	volatile uint64_t *ptr;
+
+	ptr = (volatile uint64_t *)(MAILBOX_SW_REG_BASE + offset);
+	ptr = cache_to_uncache(ptr);
+
+	return *ptr;
+}
+
+static inline void mailbox_sw_regs_write(size_t offset, const void *src, size_t bytes)
+{
+	int ret = memcpy_s((void *)(MAILBOX_SW_REG_BASE + offset),
+			   MAILBOX_SW_REG_SIZE - offset, src, bytes);
+
+	(void)ret;
+	assert(!ret);
+	dcache_writeback_region((void *)(MAILBOX_SW_REG_BASE + offset), bytes);
+}
+
+#endif /* __ACE_LIB_MAILBOX_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of platform/lib/mailbox.h"
+
+#endif /* __PLATFORM_LIB_MAILBOX_H__ */

src/platform/intel/ace/lib/dma.c
@@ -1,0 +1,198 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2022 Intel Corporation. All rights reserved.
+//
+// Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+//         Keyon Jie <yang.jie@linux.intel.com>
+//         Rander Wang <rander.wang@intel.com>
+//         Janusz Jankowski <janusz.jankowski@linux.intel.com>
+
+#include <sof/common.h>
+#include <sof/drivers/dw-dma.h>
+#include <sof/drivers/hda-dma.h>
+#include <sof/drivers/interrupt.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/memory.h>
+#include <sof/sof.h>
+#include <rtos/spinlock.h>
+
+#define DMAC0_CLASS 6
+#define DMAC1_CLASS 7
+#define DMAC_HOST_IN_CHANNELS_COUNT 7
+#define DMAC_HOST_OUT_CHANNELS_COUNT 9
+#define DMAC_LINK_IN_CHANNELS_COUNT 7
+#define DMAC_LINK_OUT_CHANNELS_COUNT 9
+
+const struct dw_drv_plat_data dmac0 = {
+	.chan[0] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[1] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[2] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[3] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[4] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[5] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[6] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+	.chan[7] = {
+		.class	= DMAC0_CLASS,
+		.weight = 0,
+	},
+};
+
+const struct dw_drv_plat_data dmac1 = {
+	.chan[0] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[1] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[2] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[3] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[4] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[5] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[6] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+	.chan[7] = {
+		.class	= DMAC1_CLASS,
+		.weight = 0,
+	},
+};
+
+SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
+{	/* Low Power GP DMAC 0 */
+	.plat_data = {
+		.id		= DMA_GP_LP_DMAC0,
+		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
+				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV,
+		.caps		= DMA_CAP_GP_LP,
+		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC |
+				  DMA_DEV_ALH,
+		.base		= LP_GP_DMA_BASE(0),
+		.channels	= 8,
+		.irq		= IRQ_EXT_LP_GPDMA0_LVL5(0),
+		.irq_name	= irq_name_level5,
+		.drv_plat_data	= &dmac0,
+	},
+	.ops		= &dw_dma_ops,
+},
+{	/* Low Power GP DMAC 1 */
+	.plat_data = {
+		.id		= DMA_GP_LP_DMAC1,
+		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
+				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV,
+		.caps		= DMA_CAP_GP_LP,
+		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC |
+				  DMA_DEV_ALH,
+		.base		= LP_GP_DMA_BASE(1),
+		.channels	= 8,
+		.irq		= IRQ_EXT_LP_GPDMA1_LVL5(0),
+		.irq_name	= irq_name_level5,
+		.drv_plat_data	= &dmac1,
+	},
+	.ops		= &dw_dma_ops,
+},
+{	/* Host In DMAC */
+	.plat_data = {
+		.id		= DMA_HOST_IN_DMAC,
+		.dir		= DMA_DIR_LMEM_TO_HMEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HOST,
+		.base		= GTW_HOST_IN_STREAM_BASE(0),
+		.channels	= DMAC_HOST_IN_CHANNELS_COUNT,
+		.chan_size	= GTW_HOST_IN_STREAM_SIZE,
+	},
+	.ops		= &hda_host_dma_ops,
+},
+{	/* Host out DMAC */
+	.plat_data = {
+		.id		= DMA_HOST_OUT_DMAC,
+		.dir		= DMA_DIR_HMEM_TO_LMEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HOST,
+		.base		= GTW_HOST_OUT_STREAM_BASE(0),
+		.channels	= DMAC_HOST_OUT_CHANNELS_COUNT,
+		.chan_size	= GTW_HOST_OUT_STREAM_SIZE,
+	},
+	.ops		= &hda_host_dma_ops,
+},
+{	/* Link In DMAC */
+	.plat_data = {
+		.id		= DMA_LINK_IN_DMAC,
+		.dir		= DMA_DIR_DEV_TO_MEM,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HDA,
+		.base		= GTW_LINK_IN_STREAM_BASE(0),
+		.channels	= DMAC_LINK_IN_CHANNELS_COUNT,
+		.chan_size	= GTW_LINK_IN_STREAM_SIZE,
+	},
+	.ops		= &hda_link_dma_ops,
+},
+{	/* Link out DMAC */
+	.plat_data = {
+		.id		= DMA_LINK_OUT_DMAC,
+		.dir		= DMA_DIR_MEM_TO_DEV,
+		.caps		= DMA_CAP_HDA,
+		.devs		= DMA_DEV_HDA,
+		.base		= GTW_LINK_OUT_STREAM_BASE(0),
+		.channels	= DMAC_LINK_OUT_CHANNELS_COUNT,
+		.chan_size	= GTW_LINK_OUT_STREAM_SIZE,
+	},
+	.ops		= &hda_link_dma_ops,
+},};
+
+const struct dma_info lib_dma = {
+	.dma_array = cache_to_uncache_init((struct dma *)dma),
+	.num_dmas = ARRAY_SIZE(dma)
+};
+
+/* Initialize all platform DMAC's */
+int dmac_init(struct sof *sof)
+{
+	int i;
+	/* no probing before first use */
+
+	/* TODO: dynamic init based on platform settings */
+
+	sof->dma_info = &lib_dma;
+
+	/* early lock initialization for ref counting */
+	for (i = 0; i < sof->dma_info->num_dmas; i++)
+		k_spinlock_init(&sof->dma_info->dma_array[i].lock);
+
+	return 0;
+}